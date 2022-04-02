c877ab803e2c40ce0945beb90f8e2975e96c26b8
itsKaspar <kaspar.ravel@gmail.com>
2022-04-02 20:17:01 +0200

    updating a lot of thinggz

--- build/quadtree.js
+++ build/quadtree.js
@@ -180,10 +180,7 @@ const Rectangle = require('./Rectangle.js'); // import from other files
 const Circle = require('./Circle.js');
 const Quadtree = require('./Quadtree.js');
 
-if(typeof window !== 'undefined') window.Q = { Quadtree, Rectangle, Circle }; // would change Q to the name of the library
+if(typeof window !== 'undefined') window.quadtree = { Quadtree, Rectangle, Circle }; // would change Q to the name of the library
 else module.exports = { Quadtree, Rectangle, Circle }; // in node would create a context
 
-
-
- 
 },{"./Circle.js":1,"./Quadtree.js":2,"./Rectangle.js":3}]},{},[4]);
--- main.js
+++ main.js
@@ -2,9 +2,5 @@ const Rectangle = require('./Rectangle.js'); // import from other files
 const Circle = require('./Circle.js');
 const Quadtree = require('./Quadtree.js');
 
-if(typeof window !== 'undefined') window.Q = { Quadtree, Rectangle, Circle }; // would change Q to the name of the library
+if(typeof window !== 'undefined') window.quadtree = { Quadtree, Rectangle, Circle }; // would change Q to the name of the library
 else module.exports = { Quadtree, Rectangle, Circle }; // in node would create a context
-
-
-
- 
