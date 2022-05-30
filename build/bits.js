3922bd684ed384603d9b4213ecc38b68b420557e
itsKaspar <kaspar.ravel@gmail.com>
2022-05-31 00:45:38 +0200

    updated window system

--- build/bits.js
+++ build/bits.js
@@ -145,15 +145,23 @@ if(typeof window !== 'undefined') window.Maths = Maths; // export for window
 module.exports = Maths; // and module
 
 },{}],3:[function(require,module,exports){
+class OS{
+	constructor(){}
+
+}
+
 class Windox{
 	constructor(o){
 
     this.content = "hello world"
     this.width = width/2;
     this.height = height/2;
-    this.pos = createVector(width/2, height/2);
-    this.BAR = 30;
+		let x = random(0 + this.width/2, width - this.width/2);
+		let y = random(0 + this.height/2, height - this.height/2);
+    this.pos = createVector(x, y);
+    this.s = 30; // space
 
+		this.display = true;
 	}
 
   //====================================================
@@ -163,6 +171,8 @@ class Windox{
   //====================================================
 
 	draw() {
+		if(this.display){
+
     rectMode(CENTER);
     fill(255,255,255);
     stroke(0,0,0);
@@ -171,17 +181,59 @@ class Windox{
     rect(this.pos.x, this.pos.y, this.width, this.height);
 
     // top fo the window
-    stroke(255,0,0);
-    line(this.pos.x - this.width/2, this.pos.y - this.height/2 + this.BAR,
-          this.pos.x + this.width/2, this.pos.y - this.height/2 + this.BAR);
+    line(this.pos.x - this.width/2, this.pos.y - this.height/2 + this.s,
+         this.pos.x + this.width/2, this.pos.y - this.height/2 + this.s);
+
+		//draw cross
+		drawCross(this.pos.x + this.width/2 - this.s/2,
+							this.pos.y - this.height/2 + this.s/2,
+							this.s/4);
 
     // draw content of the window
     //text(0,0,""test");
-
+		}
 	}
 
   //====================================================
 
+  // ACTIONS
+
+  //====================================================
+
+	mousePressed(){
+		// if mouse is pressed on X
+		if(	mouseX > this.pos.x + this.width/2 - this.s && mouseX < this.pos.x + this.width/2 + this.s &&
+				mouseY > this.pos.y - this.height/2 - this.s && mouseY < this.pos.y - this.height/2 + this.s){
+			this.close();
+		}
+		// if mouse is pressed somewhere else
+	}
+
+	mouseDragged(){
+		// if mouse is pressed on X
+		if(	mouseX > this.pos.x - this.width/2 && mouseX < this.pos.x + this.width/2 &&
+				mouseY > this.pos.y - this.height/2 && mouseY < this.pos.y - this.height/2 + this.s){
+			this.move();
+		}
+	}
+
+  move(){
+		this.pos.x += (mouseX - pmouseX);
+		this.pos.y += (mouseY - pmouseY);
+  }
+
+	close(){
+		this.display = false;
+	}
+
+}
+
+function drawCross(cx, cy, w){
+	// draw cross
+	//descending line
+	line(cx - w, cy + w, cx + w, cy - w);
+	//ascending line
+	line(cx - w, cy - w, cx + w, cy + w);
 }
 
 if(typeof window !== 'undefined') window.Windox = Windox; // export for window
